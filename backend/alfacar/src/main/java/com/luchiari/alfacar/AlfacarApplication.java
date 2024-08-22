1af91d01524dcb48a1091d63c06b11e544251ecb
MathLuchiari <mm.luchiari2002@gmail.com>
2024-08-21 21:01:24 -0300

    Commit inicial

---
+++ backend/alfacar/src/main/java/com/luchiari/alfacar/AlfacarApplication.java
@@ -0,0 +1,13 @@
+package com.luchiari.alfacar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfacarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlfacarApplication.class, args);
+	}
+
+}
---
+++ backend/alfacar/src/test/java/com/luchiari/alfacar/AlfacarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luchiari.alfacar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfacarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
