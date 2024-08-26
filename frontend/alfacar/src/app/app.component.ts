a07a04d25e7ebb947a1b095ba87ddba21870bba3
MathLuchiari <mm.luchiari2002@gmail.com>
2024-08-25 22:38:59 -0300

    Inicio dev side menu

--- frontend/alfacar/src/app/app.component.ts
+++ frontend/alfacar/src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'alfacar';
+
+  @ViewChild('side_menu') sideMenu: any;
+
+  toggleSideMenu(): void {
+    this.sideMenu
+  }
 }
