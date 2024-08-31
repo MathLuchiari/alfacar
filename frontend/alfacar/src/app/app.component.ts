1b4ede59f87faf44e58c85b5107eec641cc48f22
MathLuchiari <mm.luchiari2002@gmail.com>
2024-08-31 01:10:52 -0300

    Mudança de lib di Angular Material para PO-UI

--- frontend/alfacar/src/app/app.component.ts
+++ frontend/alfacar/src/app/app.component.ts
@@ -1,60 +1,52 @@
-import { Component, ViewChild } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 
+import {
+  PoMenuItem,
+  PoMenuModule,
+  PoPageModule,
+  PoToolbarModule,
+} from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    CommonModule,
     RouterOutlet,
-    MatSidenavModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule
+    PoToolbarModule,
+    PoMenuModule,
+    PoPageModule
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'alfacar';
-
-  isMenuExpanded: boolean = false;
-
-  toggleSideMenu(): void {
-    const sideMenu = document.getElementsByClassName('app__side-menu')[0];
-    const listBtnTexts: HTMLCollectionOf<HTMLElement> = document.getElementsByClassName('app__btn--side-menu-text') as HTMLCollectionOf<HTMLElement>;
-    const listBtnIcons: HTMLCollectionOf<HTMLElement> = document.getElementsByClassName('app__btn--side-menu-icon') as HTMLCollectionOf<HTMLElement>;
-
-    if( this.isMenuExpanded ) {
-      sideMenu.classList.add('app__side-menu--expanded')
-
-      for (let i=0; i < listBtnTexts.length; i++) {
-        const btnText: HTMLElement = listBtnTexts[i];
-        btnText.style['display'] = '';
-      }
-
-      for (let i=0; i < listBtnIcons.length; i++) {
-        const btnIcon: HTMLElement = listBtnIcons[i];
-        btnIcon.classList.remove('app__btn--side-menu-icon--centered')
-      }
-    } else {
-      sideMenu.classList.remove('app__side-menu--expanded')
-
-      for (let i=0; i < listBtnTexts.length; i++) {
-        const btnTexts: HTMLElement = listBtnTexts[i];
-        btnTexts.style['display'] = 'none'
-      }
-
-      for (let i=0; i < listBtnIcons.length; i++) {
-        const btnIcon: HTMLElement = listBtnIcons[i];
-        btnIcon.classList.add('app__btn--side-menu-icon--centered')
-      }
+  readonly menus: Array<PoMenuItem> = [
+    {
+      label: 'Home',
+      action: this.onClick.bind(this, 'home'),
+      icon: 'ph ph-house',
+      shortLabel: 'Home'
+    },
+    {
+      label: 'Dados Oficina',
+      action: this.onClick.bind(this, 'workshops/new'),
+      icon: 'ph ph-wrench',
+      shortLabel: 'Oficina'
     }
+  ];
 
-    this.isMenuExpanded = !this.isMenuExpanded;
+  menuSelectedTitle: string = "";
+
+  constructor(
+    private router: Router
+  ) {}
+
+  private onClick(page: string) {
+    // this.menuSelectedTitle = ;
+
+    this.router.navigate([page]);
   }
 }
