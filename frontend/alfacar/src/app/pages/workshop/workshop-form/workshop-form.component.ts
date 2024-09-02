4ebdb37ccaba142a0dae168e499940a0621643fe
MathLuchiari <mm.luchiari2002@gmail.com>
2024-09-01 21:18:22 -0300

    Inicio criação form oficina, prob busca cep

--- frontend/alfacar/src/app/pages/workshop/workshop-form/workshop-form.component.ts
+++ frontend/alfacar/src/app/pages/workshop/workshop-form/workshop-form.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-
+import { PoPageModule, PoBreadcrumb, PoDialogService, PoNotificationService, PoPageEditLiterals, PoFieldModule, PoDividerModule  } from '@po-ui/ng-components';
 @Component({
   selector: 'app-workshop-form',
   standalone: true,
-  imports: [],
+  imports: [
+    PoPageModule,
+    PoFieldModule,
+    PoDividerModule
+  ],
   templateUrl: './workshop-form.component.html',
   styleUrl: './workshop-form.component.scss'
 })
 export class WorkshopFormComponent {
 
+  onCancel(): void {
+
+  }
+
+  onSave(): void {
+
+  }
 }
