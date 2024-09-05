7547352ee9eff4fa67dced6f5c178b8bdc638084
MathLuchiari <mm.luchiari2002@gmail.com>
2024-09-05 20:11:56 -0300

    Inicio da criação da listagem de oficinas

--- frontend/alfacar/src/app/pages/workshop/workshop-list/workshop-list.component.ts
+++ frontend/alfacar/src/app/pages/workshop/workshop-list/workshop-list.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PoPageModule, PoTableColumn, PoTableModule } from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-workshop-list',
   standalone: true,
-  imports: [],
+  imports: [
+    PoPageModule,
+    PoTableModule
+  ],
   templateUrl: './workshop-list.component.html',
   styleUrl: './workshop-list.component.scss'
 })
-export class WorkshopListComponent {
+export class WorkshopListComponent implements OnInit {
+  columns!: Array<PoTableColumn>;
 
+  ngOnInit(): void {
+    this.setColumns();
+  }
+
+  setColumns(): void {
+    this.columns = [
+      { property: 'id', label: 'Código', type: 'link', action: (row: string) => this.goToView(row) },
+      { property: 'name', label: 'Nome' },
+      { property: 'rg', label: 'RG', visible: false },
+      { property: 'cpf', label: 'CPF' },
+      { property: 'email', label: 'E-mail' },
+      { property: 'tel1', label: 'Telefone 1' },
+      { property: 'tel2', label: 'Telefone 2', visible: false },
+      { property: 'pets', label: 'Pets', type: 'icon', icons: [
+        { value: 'view-pet', icon: 'po-icon-eye', action: (owner: Owner) => this.goToViewPets(owner.id), tooltip: 'Visualizar pets' },
+        { value: 'include-pet', icon: 'po-icon-plus-circle', action: (owner: Owner) => this.goToPetForm(owner.id), tooltip: 'Incluir pets' }
+      ] }
+    ];
+  }
 }
